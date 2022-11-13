d61d2732cd30044cadf8d3470f11c1123dbe58fb
Alexey Sen <alexey.sen@gmail.com>
2022-11-13 14:03:11 +0300

    hystrix is ok but hystrix dashboard not working

--- src/main/java/com/example/clientservice/ClientServiceApplication.java
+++ src/main/java/com/example/clientservice/ClientServiceApplication.java
@@ -2,16 +2,20 @@ package com.example.clientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableHystrix
 public class ClientServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/clientservice/service/ClientService.java
+++ src/main/java/com/example/clientservice/service/ClientService.java
@@ -3,11 +3,15 @@ package com.example.clientservice.service;
 import com.example.clientservice.connector.FeignConnector;
 import com.example.clientservice.connector.RestTemplateConnector;
 import com.example.clientservice.model.Book;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class ClientService {
@@ -15,11 +19,23 @@ public class ClientService {
     private FeignConnector feignConnector;
     private RestTemplateConnector restTemplateConnector;
 
+    @HystrixCommand(fallbackMethod = "failed")
     public List<Book> getAllBooks() {
         return feignConnector.getAllBooksList();
     }
 
     public List<Book> data() {
         return restTemplateConnector.data();
+    }
+
+    /**
+     * Hystrix fallback method
+     * @return
+     */
+    public List<Book>  failed() {
+        String error = "Service is not available now. Please try again later";
+        log.info(error);
+        List<Book> books = new ArrayList<>();
+        return books;
     }
 }
